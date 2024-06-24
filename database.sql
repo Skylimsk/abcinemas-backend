b5a9c6bbbf4b5f25d82cb911bf4eac66fbde62be
MJ <chaijing13691@gmail.com>
2024-06-25 02:42:22 +0800

    up db

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ USE abcinema;
 CREATE TABLE user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     full_name VARCHAR(100)  NOT NULL,
     date_of_birth DATE  NOT NULL,
     phone_number VARCHAR(20)  NOT NULL,
